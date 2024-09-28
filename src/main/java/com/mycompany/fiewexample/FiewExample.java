d338321efe5a6702e0617e54565d2c2a9dc5e6f4
Bakr <mbakr9212@gmail.com>
2024-09-29 01:10:27 +0300

    Array List

--- src/main/java/com/mycompany/fiewexample/FiewExample.java
+++ src/main/java/com/mycompany/fiewexample/FiewExample.java
@@ -4,30 +4,36 @@
 
 package com.mycompany.fiewexample;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 /**
  *
  * @author Lenovo
  */
 public class FiewExample {
 
-    static int add(int n1,int n2){
-        int n3 = n1+n2;
-        return n3;
-    }
+   public static void main (String []args) {
+       
+       ArrayList  <String> list=new ArrayList<String>();//by default the size is 10 >>()
+   list.add("Ahmed");
+   list.add("Mohamed");
+   list.add("Omar");
+   list.add("Ali");
+   list.add("Khaled");
+       System.out.println("The size of array is "+ list.size());
+       System.out.println("Will show the values in array:"+list);
+       System.out.println("هيطبع القيمه الي محدده"+list.get(3));
+       
+       list.add(0, "Abduallah");//Enter a variable other than this 
+       list.set(3,"Eyad");
+        list.remove(1);
+        list.remove(2);
 
-    public static void main(String[] args) {
-        Math n1=new Math(9);
-        Math n2=new Math(5);
-        Math n3=Math.add(n1, n2);
-        System.out.println("The sum of 2 function by the second way :" + n3.val);
-   // or 
-   Math n5=new Math(3);
-   Math n6=new Math(32);
-   Math n7=n6.add(n5);
-   System.out.println("The sum of 2 function by the Third way:"+n7.val);;
-   
-
-
+        
+        System.out.println(list.size());
+        System.out.println("طبعنا الاراي بعدماعدلنا عليها"+list);   
+       
+    }      
       
- }
 }
